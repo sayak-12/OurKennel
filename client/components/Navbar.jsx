57fad8c00778b50f3e8b2dd08d4c1d0b46620988
sayak raha <sayakraha12@gmail.com>
2023-10-29 12:27:17 +0530

    commit

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,53 +1,111 @@
-import './Nav.scss';
-import { useDarkMode } from '../hooks/DarkmodeProvider';
-
+import "./Nav.scss";
+import { useDarkMode } from "../hooks/DarkmodeProvider";
+import { useState, useEffect } from "react";
 const Navbar = () => {
   const { darkmode, toggleDarkmode } = useDarkMode();
-  
+  const [placeholder, setPlaceholder] = useState("Search");
+  const [mobile, setMobile] = useState(false);
+  const handleFocus = () => {
+    setPlaceholder("");
+  };
+  const toggleMobile = () =>{
+    
+  }
+  const handleBlur = () => {
+    setPlaceholder("Search");
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 992) {
+        setMobile(true);
+      } else {
+        setMobile(false);
+      }
+    };
+    // Add an event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Call handleResize initially
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
-    <nav className={`navbar navbar-expand-lg ${darkmode ? 'dark' : ''}`}>
-      <div className={`container-md ${darkmode ? 'dark' : ''}}`}>
+    <nav className={`navbar navbar-expand-lg ${darkmode ? "dark" : ""}`}>
+      <div className={`container-sm ${darkmode ? "dark" : ""}}`}>
         <a className="navbar-brand" href="#">
           OurKennel
         </a>
-        <button
-          className="navbar-toggler text-white" 
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+        <div
+          className={`nav-record ${mobile ? `` : `show`}`}
+          id="navbarSupportedContent"
         >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mb-2 me-auto mb-lg-0 list">
             <li className="nav-item">
-              <a className={`nav-link ${darkmode ? 'text-white' : 'text-black'}`} aria-current="page" href="#">
+              <a
+                className={`nav-link ${darkmode ? "text-white" : "text-black"}`}
+                aria-current="page"
+                href="#"
+              >
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className={`nav-link ${darkmode ? 'text-white' : 'text-black'}`} href="#">
-                Link
+              <a
+                className={`nav-link ${darkmode ? "text-white" : "text-black"}`}
+                href="#"
+              >
+                adopt
               </a>
             </li>
-            
+            <li className="nav-item">
+              <a
+                className={`nav-link ${darkmode ? "text-white" : "text-black"}`}
+                href="#"
+              >
+                petbook
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className={`nav-link ${darkmode ? "text-white" : "text-black"}`}
+                href="#"
+              >
+                buy/sell
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className={`nav-link ${darkmode ? "text-white" : "text-black"}`}
+                href="#"
+              >
+                contact us
+              </a>
+            </li>
           </ul>
           <form className="d-flex" role="search">
             <input
-              className="inp me-2 rounded-pill outlined"
+              className="inp me-2 px-3 rounded-pill outlined"
               type="search"
-              placeholder="Search"
+              placeholder={placeholder}
               aria-label="Search"
+              onFocus={handleFocus}
+              onBlur={handleBlur}
             />
             <button className="btn btn-warning rounded-pill" type="submit">
               Search
             </button>
           </form>
+          <span
+            className="material-symbols-outlined mx-3 sun"
+            title={`Switch to ${darkmode ? "light mode" : "dark mode"}`}
+            onClick={toggleDarkmode}
+          >{`${darkmode ? "light_mode" : "dark_mode"}`}</span>
         </div>
-        <span className="material-symbols-outlined mx-3 sun" title={`Switch to ${darkmode ? 'light mode' : 'dark mode'}`} onClick={toggleDarkmode}>{`${darkmode ? 'light_mode' : 'dark_mode'}`}</span>
+        <div className={`toggler ${mobile ? `show` : ``}`} onClick={toggleMobile}>
+          <span className="material-symbols-outlined">menu</span>
+        </div>
       </div>
     </nav>
   );
